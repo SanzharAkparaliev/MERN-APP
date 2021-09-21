0ce4eaeb7b8e5c65ec18094f85ce07e0f137cae9
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-09-21 19:54:01 +0600

    Form Validation

---
+++ server/routes/posts.js
@@ -0,0 +1,16 @@
+const {Router} = require('express')
+const router = Router()
+const {Posts} = require('../models')
+
+router.get('/',async(req,res) => {
+    const listOfPosts = await Posts.findAll()
+    res.json(listOfPosts)
+})
+
+router.post('/',async(req,res) =>{
+    const post = req.body
+    await Posts.create(post)
+    res.json(post)
+})
+
+module.exports = router
