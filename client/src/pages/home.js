7aeeb70cd255d41b5cf282e1c5a129afd89ccde4
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-09-23 08:03:35 +0600

    Comment Section in React

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -16,7 +16,7 @@ function Home() {
     <div className="App">
       {listOfPosts.map((value, key) => {
         return (
-          <div className="post" onClick={() => history.push(`/post/${value.id}`) }>
+          <div key={key} className="post" onClick={() => history.push(`/post/${value.id}`) }>
             <div className="title"> {value.title} </div>
             <div className="body">{value.postText}</div>
             <div className="footer">{value.username}</div>
