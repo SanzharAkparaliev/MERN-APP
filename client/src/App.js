4d05fadb746d7d997ff1b8344b497a8b659f4a83
SanzharAkparaliev <taalaibekuulusanzhar@gmail.com>
2021-09-24 20:29:54 +0600

    logging out

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,77 @@
-import './App.css'
-import {BrowserRouter as Router,Route,Switch,Link } from "react-router-dom"
-import Home from "./pages/home"
-import CreatePost from "./pages/CreatePost"
-import Post from "./pages/Post"
-import Login from "./pages/Login"
-import Registration from "./pages/Registration"
-import {AuthContext} from './helpers/AuthContext'
-import {useState,useEffect} from 'react'
-import axios from 'axios'
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./pages/home";
+import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import { AuthContext } from "./helpers/AuthContext";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [authState,setAuthState] = useState(false)
+  const [authState, setAuthState] = useState({
+    username: "",
+    id: 0,
+    status: false,
+  });
+
   useEffect(() => {
-      axios.get('http://localhost:3001/auth/auth',{headers:{
-        accessToken:localStorage.getItem("accessToken")
-      }}).then((response) =>{
-        if(response.data.error){ setAuthState(false)}else{
-                setAuthState(true)
-        }
+    axios
+      .get("http://localhost:3001/auth/auth", {
+        headers: {
+          accessToken: localStorage.getItem("accessToken"),
+        },
       })
-    
-  },[])
+      .then((response) => {
+        if (response.data.error) {
+          setAuthState({ ...authState, status: false });
+        } else {
+          setAuthState({
+            username: response.data.username,
+            id: response.data.id,
+            status: true,
+          });
+        }
+      });
+  }, []);
 
-  return  <div className="App">
-      <AuthContext.Provider value={{authState,setAuthState}}>
-              <Router>
-                <div className="banner">       
-                     <Link to="/">Home Page</Link>
-                     <Link to="/createpost">Create A Post</Link>
-                     {!authState &&(
-                       <>
-                     <Link to="/login">Login</Link>
-                     <Link to="/registration">Registration</Link>
-                      </>
-                     )}
-                 </div>
-               <Switch>
-                  <Route path="/" exact component={Home} />
-                  <Route path="/createpost" exact component={CreatePost} />
-                  <Route path="/post/:id" exact component={Post} />
-                  <Route path="/login" exact component={Login} />
-                  <Route path="/registration" exact component={Registration} />
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({ username: "", id: 0, status: false });
+  };
 
-                </Switch>
-              </Router>
-      </AuthContext.Provider>
+  return (
+    <div className="App">
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <Router>
+          <div className="navbar">
+            <div className="links">
+              <Link to="/"> Home Page</Link>
+              <Link to="/createpost"> Create A Post</Link>
+              {!authState.status && (
+                <>
+                  <Link to="/login"> Login</Link>
+                  <Link to="/registration"> Registration</Link>
+                </>
+              )}
+            </div>
+            <div className="loggedInContainer">
+              <h1>{authState.username} </h1>
+              {authState.status && <button onClick={logout}> Logout</button>}
+            </div>
           </div>
-    
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/createpost" exact component={CreatePost} />
+            <Route path="/post/:id" exact component={Post} />
+            <Route path="/registration" exact component={Registration} />
+            <Route path="/login" exact component={Login} />
+          </Switch>
+        </Router>
+      </AuthContext.Provider>
+    </div>
+  );
 }
 
 export default App;
